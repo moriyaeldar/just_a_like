b9c60f0e3a2fa11178471933208cd2ef40089e39
zohar <zoarb100@gmail.com>
2021-10-26 21:00:35 +0300

    zohar - add user model (server)

---
+++ server/models/user.model.ts
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const {model, Schema} = mongoose;
+
+const userSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String,
+        required: true
+    },
+    linkedin_url: {
+        type: String,
+        required: true
+    },
+    experties: {
+        type: Array,
+        required: true
+    },
+    interests: {
+        type: Array,
+        required: true
+    },
+    level: {
+        type: String,
+        default: '4',
+        required: true
+    }
+});
+
+module.exports = model('User', userSchema);
