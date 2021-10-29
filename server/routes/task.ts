076ebd87acf2f77e5152a818ff9f89b94eb98fa2
barnachum203 <barnachum203@gmail.com>
2021-10-29 14:06:43 +0300

    update post task

--- server/routes/task.ts
+++ server/routes/task.ts
@@ -3,7 +3,7 @@ const router = Router();
 const taskController = require("../controllers/taskController");
 
 router.get("/", taskController.index);
-router.post("/", taskController.createTask);
+router.post("/pid", taskController.createTask);
 router.delete("/:tid", taskController.deleteTask);
 router.put("/:pid/:tid", taskController.updateTask);
 router.get("/:pid/:tid", taskController.getAssociatedTasks);
