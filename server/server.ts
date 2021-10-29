f582ac3fbe6de207541d31535646d0881d90fd61
barnachum203 <barnachum203@gmail.com>
2021-10-29 14:05:10 +0300

    Added body-parser

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 const app: Application = express();
 
+app.use(require('body-parser').json())
 require("dotenv").config();
 // require("./startup/db")();
 require("./startup/routes")(app);
