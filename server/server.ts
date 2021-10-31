987b24d507ad80779c382fb99a09707c7789275a
Sharel Shalom <sharel1155@gmail.com>
2021-10-31 22:12:56 +0200

    Seed

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,8 @@
 import express, { Application } from "express";
 const app: Application = express();
 
-app.use(require('body-parser').json())
 require("dotenv").config();
-// require("./startup/db")();
+require("./startup/db").connect();
 require("./startup/routes")(app);
 // require("./startup/seed")();
 
