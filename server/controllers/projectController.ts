e7990cdb16c2a6605caa7ffb93a983d57bd3bee4
Sharel Shalom <sharel1155@gmail.com>
2021-10-31 21:44:04 +0200

    Project1

--- server/controllers/projectController.ts
+++ server/controllers/projectController.ts
@@ -43,18 +43,22 @@ module.exports.getSpecificProject = catchAsync(async (req: Request, res: Respons
  * */
 module.exports.createProject = catchAsync(async (req: Request, res: Response) => {
     try{
-      let {error} = validateProject(req.body);
-      if(error) return res.status(400).json(error.details[0].message);
-
-      //Check the users that will be project manager
-      const projectMangers = req.params.projectManagers;
-      for(let i=0; i < projectMangers.length; i++) {
-          let user = await User.findById(projectMangers[i]);
-          if(!user) 
-            return res.status(404).json(`The user with the id: ${projectMangers[i]} does not exist.`);
-          if(user.level > 2) //Check the permission level
-            return res.status(401).json(`The user with id:  ${projectMangers[i]} is not allowed to be project manager.`);
-      }
+      /**
+       * ---Validation of project and users---
+       let {error} = validateProject(req.body);
+       if(error) return res.status(400).json(error.details[0].message);
+ 
+       //Check the users that will be project manager
+       const projectMangers = req.params.projectManagers;
+       for(let i=0; i < projectMangers.length; i++) {
+           let user = await User.findById(projectMangers[i]);
+           if(!user) 
+             return res.status(404).json(`The user with the id: ${projectMangers[i]} does not exist.`);
+           if(user.level > 2) //Check the permission level
+             return res.status(401).json(`The user with id:  ${projectMangers[i]} is not allowed to be project manager.`);
+       }
+       * 
+       */
 
       const project = new Project({
         name: req.body.name,
