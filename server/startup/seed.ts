f794219a5e2ef5f5ff1376c4a90221504f66db39
barnachum203 <barnachum203@gmail.com>
2021-11-01 18:50:41 +0200

    added seeds

--- server/startup/seed.ts
+++ server/startup/seed.ts
@@ -1,41 +1,58 @@
-// require the necessary libraries
 const faker = require("faker");
-const mongoose = require("mongoose");
-const Expertise = require("../models/expertise.model");
 const Task = require("../models/task.model");
+const User = require("../models/user.model");
+const mongoose = require("mongoose");
 
-module.exports = async function seedDB() {
-  try {
-    // The drop() command destroys all data from a collection.
-    // Make sure you run it against proper database and collection.
-    await Expertise.deleteMany();
-    await Task.deleteMany();
-    let expertises = [];
-    let tasks = [];
-    for (let i = 0; i < 15; i++) {
-      const newTask = {
-        name: faker.name.firstName(),
-        description: faker.lorem.words(),
-        status: faker.datatype.number(100),
-        users: [mongoose.Types.ObjectId()],
-        startDate: Date.now(),
-        finishDate: Date.now(),
-      };
-
-      const newExpertise = {
-        name: faker.lorem.words(1)
-      }
-
-      expertises.push(newExpertise);
-
-      tasks.push(newTask);
-    }
-    await Expertise.create(expertises);
-    console.log("Database seeded with expertises! :)");
-
-    await Task.create(tasks);
-    console.log("Database seeded with tasks! :)");
-  } catch (err) {
-    console.log(err);
+async function seedTasks() {
+  const res = await Task.deleteMany();
+  if (res) {
+    console.log("Tasks Deleted Successfully");
   }
-};
+  let tasks = [];
+
+  for (let i = 0; i < 15; i++) {
+    const newTask = {
+      name: faker.name.firstName(),
+      description: faker.lorem.words(),
+      status: faker.datatype.number(100),
+      users: [
+        mongoose.Types.ObjectId(),
+        mongoose.Types.ObjectId(),
+        mongoose.Types.ObjectId(),
+      ],
+      startDate: Date.now(),
+      finishDate: Date.now(),
+    };
+
+    tasks.push(newTask);
+  }
+  // tasks:Task
+  await Task.create(tasks);
+  console.log("Tasks Created");
+}
+
+async function seedUsers() {
+  const res = await User.deleteMany();
+  if (res) {
+    console.log("Users Deleted Successfully");
+  }
+  let users: any = [];
+
+  for (let i = 0; i < 15; i++) {
+    const newUser = {
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email(),
+      phone_number: faker.datatype.number(900000),
+      linkedin_url: faker.internet.email(),
+      experties: ["617fcc43e6aad249c573c757"],
+      interests: ["617fcc43e6aad249c573c758"],
+      level: 4,
+    };
+
+    users.push(newUser);
+  }
+  await User.create(users);
+  console.log("Users Created");
+}
+module.exports = { seedTasks, seedUsers };
