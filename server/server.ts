b2ee13c4ef4b04eca8331d07364dcf22a2402fb6
barnachum203 <barnachum203@gmail.com>
2021-11-03 14:42:31 +0200

    Added commented seeders

--- server/server.ts
+++ server/server.ts
@@ -7,13 +7,17 @@ require("./startup/routes")(app);
 const seeds = require("./startup/seed");
 
 //Uncomment to seed db
-seeds.seedTasks();
+// seeds.seedExperties();
+// seeds.seedInterests();
 // seeds.seedUsers();
+// seeds.seedTasks();
+// seeds.seedProjects();
+// seeds.seedTeams();
+
+// Seed all DB
+// seeds.seedDB();
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log("Connect succesfully on port: " + port);
 });
-
-
-    
