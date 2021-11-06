60cfc7235a99a00d69537344c5b02fbd90d3a182
barnachum203 <barnachum203@gmail.com>
2021-11-06 16:48:18 +0200

    Added get Associated Tasks In Project

--- server/api/task/taskController.ts
+++ server/api/task/taskController.ts
@@ -1,6 +1,7 @@
 import { Response, Request } from "express";
 const catchAsync = require("../../utillities/catchAsync");
 const Task = require("../../models/task.model");
+const Project = require("../../models/project.model");
 
 /*************************************************************
  ** All Routes taken from "requirments" doc. *****************
@@ -54,9 +55,10 @@ module.exports.deleteTask = catchAsync(async (req: Request, res: Response) => {
   const result = await Task.findByIdAndDelete(tid);
   console.log(`[TASK-REMOVE] - removed task_id: ${tid}`);
   console.log(result);
-  if (result){ res.send("Deleted task: " + tid);}
-  else{
-    res.send("Task not in DB: " + tid);
+  if (result) {
+    res.send("Deleted task: " + tid);
+  } else {
+    res.send("Task not found in DB: " + tid);
   }
 });
 
@@ -78,20 +80,20 @@ module.exports.updateTask = catchAsync(async (req: Request, res: Response) => {
   const filter = { _id: req.params.tid };
   //Fins task by id and update entire object
   const doc = await Task.findByIdAndUpdate(filter, updatedTask);
-  console.log(
-    `[TASK-UPDATE] - update task_id: ${tid}, ${JSON.stringify(updatedTask)} `
-  );
-  res.send(`Task updated: \n${JSON.stringify(updatedTask)}`);
+  if (doc == null) {
+    res.send(`No document found for task id: ${tid}`);
+  } else {
+    console.log(
+      `[TASK-UPDATE] - update task_id: ${tid}, ${JSON.stringify(updatedTask)} `
+    );
+    res.send(`Task updated: \n${updatedTask}`);
+  }
 });
 
 /**
  * Get all Associated tasks for user:
  * Require user level 3-1
- * to get associated tasks.
- *
- * Level 3 - get he's associated tasks in associated project.
- * Level 2 - get he's associated tasks in associated project.
- * Level 1 - get all the tasks in the project.
+ * to get associated all tasks.
  *
  * @param:
  * uid - user id
@@ -102,7 +104,59 @@ module.exports.getAssociatedTasks = catchAsync(
     const uid = req.params.uid;
     //Find user id in Task collection and return every associated task.
     const result = await Task.find({ users: { $in: uid } });
-    console.log(`[ASSOCIATE-TASK] found ${result.length} tasks for id: ${uid}`);
-    res.send(result);
+    if (result.length == 0) {
+      res.send(`No asssociated tasks found for user id: ${uid}`);
+    } else {
+      console.log(
+        `[ALL-ASSOCIATED-TASK] found ${result.length} tasks for id: ${uid}`
+      );
+      res.send(result);
+    }
+  }
+);
+
+/**
+ * Get all Associated tasks for user in a specific project:
+ * Require user level 3-1
+ * to get associated tasks.
+ *
+ * Level 3 - get he's associated tasks in associated project.
+ * Level 2 - get he's associated tasks in associated project.
+ * Level 1 - get all the tasks in the project.
+ *
+ * @param:
+ * uid - user id
+ * pid - project id
+ *
+ * */
+module.exports.getAssociatedTasksInProject = catchAsync(
+  async (req: Request, res: Response) => {
+    //TODO: Add permission control
+    const uid = req.params.uid;
+    const pid = req.params.pid;
+
+    //Get all associated tasks id's for specific project.
+    const tasks = await Project.findById({ _id: pid }, "tasks");
+
+    const associatedTasks: any[] = [];
+    // Get all task that associated to the user from the project id
+    for (let i = 0; i < tasks.tasks.length; i++) {
+      let result = await Task.findById({ _id: tasks.tasks[i] });
+      //TODO: If Admin reconized - return array of all results.
+      if (result) {
+        //Check if res contain the uid
+        if (result.users.includes(uid)) {
+          associatedTasks.push(result);
+        }
+      }
+    }
+    if (associatedTasks.length == 0) {
+      res.send(`No asssociated tasks found for user id: ${uid}`);
+    } else {
+      console.log(
+        `[ASSOCIATE-TASK-BY-ID] found ${associatedTasks.length} tasks for \nid: ${uid}\nin project id: ${pid}`
+      );
+      res.send(associatedTasks);
+    }
   }
 );
