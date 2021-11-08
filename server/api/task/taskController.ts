57a74c936b7df693b34a98fc197621d45e34caa1
barnachum203 <barnachum203@gmail.com>
2021-11-08 22:41:09 +0200

    Added permissions control updated all functions to support the middleware

--- server/api/task/taskController.ts
+++ server/api/task/taskController.ts
@@ -2,6 +2,7 @@ import { Response, Request } from "express";
 const catchAsync = require("../../utillities/catchAsync");
 const Task = require("../../models/task.model");
 const Project = require("../../models/project.model");
+const User = require("../../models/user.model");
 
 /*************************************************************
  ** All Routes taken from "requirments" doc. *****************
@@ -23,33 +24,36 @@ module.exports.allTasks = catchAsync(async (req: Request, res: Response) => {
 
 /**
  * Create tasks:
- * Require user level 1 (-Admin)
+ * @access Require user level 1 (-Admin)
  * to create new task
  * */
 module.exports.createTask = catchAsync(async (req: Request, res: Response) => {
-  //TODO: Add permission control
-
-  const newTask = req.body;
+  const { name, description, status, users, startDate, finishDate } = req.body;
 
   //Create new task
-  const result = await new Task(newTask);
-  result.save();
+  const newTask = new Task({
+    name,
+    description,
+    status,
+    users,
+    startDate,
+    finishDate,
+  });
+  let task = await newTask.save();
 
   console.log(`[TASK-CREATE]: added new task to DB`);
 
-  if (result) res.send("Added new task: " + newTask);
+  if (newTask) res.send("Added new task: " + task);
 });
 
 /**
  * Delete tasks:
- * Require user level 1 (-Admin)
+ * @access Require user level 1 (-Admin)
  * to delete existing task
  *
- * @param:
- * tid - task id
+ * @param tid task id
  * */
 module.exports.deleteTask = catchAsync(async (req: Request, res: Response) => {
-  //TODO: Add permission control
   const tid = req.params.tid;
   //Delete task by task id
   const result = await Task.findByIdAndDelete(tid);
@@ -67,38 +71,80 @@ module.exports.deleteTask = catchAsync(async (req: Request, res: Response) => {
  * Require user level 3 or 1 (Volunteer / Admin)
  * to update existing task.
  *
- * Level 3 - can update task only if he is associated with the project.
- * Level 1 - can update any task in any project.
+ * @access Level 3 - can update task only if he is associated with the project.
+ * @access Level 1 - can update any task in any project.
  *
- * @param:
- * tid - task id
+ * @param tid task id
+ * @augments _id user id in request body
+ * @augments projectId project id in request body
  * */
 module.exports.updateTask = catchAsync(async (req: Request, res: Response) => {
-  //TODO: Add permission control
+  const { _id } = req.body.user || req.body || null;
+  const { projectId } = req.body || req.body.project || null;
   const tid = req.params.tid;
-  const updatedTask = req.body;
-  const filter = { _id: req.params.tid };
-  //Fins task by id and update entire object
-  const doc = await Task.findByIdAndUpdate(filter, updatedTask);
-  if (doc == null) {
-    res.send(`No document found for task id: ${tid}`);
+  var user: any = {};
+  var project: any = {};
+
+  project = await Project.find({ _id: projectId }, (error: any, data: any) => {
+    if (error) {
+      console.log(`Failed to find project ${projectId}:\n ${error}`);
+    } else {
+      return data;
+    }
+  }).clone();
+
+  user = await User.find({ _id: _id }, (error: any, data: any) => {
+    if (error) {
+      console.log(`Failed to find user ${_id}:\n ${error}`);
+    } else {
+      return data;
+    }
+  }).clone();
+
+  //Check if user associated with the project and has permission to update task
+  if (
+    (user[0].level == "3" && project[0].tasks.toString().includes(tid)) ||
+    user[0].level == "1"
+  ) {
+    const { name, description, status, users, startDate, finishDate } =
+      req.body;
+
+    //Get the task required to update
+    const task = await Task.findById(tid);
+    if (task) {
+      task.name = name;
+      task.description = description;
+      task.status = status;
+      task.users = users || [];
+      task.startDate = startDate;
+      task.finishDate = finishDate;
+    }
+
+    //Save to DB
+    const updatedTask = await task.save();
+
+    if (updatedTask) {
+      console.log(
+        `[TASK-UPDATE] - user ${_id} updated task_id: ${tid}, ${updatedTask} `
+      );
+      res.send(`Task updated: \n${updatedTask}`);
+    } else {
+      res.send(`No document found for task id: ${tid}`);
+    }
   } else {
-    console.log(
-      `[TASK-UPDATE] - update task_id: ${tid}, ${JSON.stringify(updatedTask)} `
-    );
-    res.send(`Task updated: \n${updatedTask}`);
+    console.log(`Not permitted to update [level]: ${user[0].level}`);
+    res.send(`Not permitted to update task: ${tid}`);
   }
 });
 
 /**
  * Get all Associated tasks for user:
- * Require user level 3-1
+ * @access Require user level 3-1
  * to get associated all tasks.
  *
- * @param:
- * uid - user id
+ * @param uid user id
  * */
-module.exports.getAssociatedTasks = catchAsync(
+module.exports.getAllAssociatedTasks = catchAsync(
   async (req: Request, res: Response) => {
     //TODO: Add permission control
     const uid = req.params.uid;
@@ -120,18 +166,16 @@ module.exports.getAssociatedTasks = catchAsync(
  * Require user level 3-1
  * to get associated tasks.
  *
- * Level 3 - get he's associated tasks in associated project.
- * Level 2 - get he's associated tasks in associated project.
- * Level 1 - get all the tasks in the project.
+ * @access Level 3 - get he's associated tasks in associated project.
+ * @access Level 2 - get he's associated tasks in associated project.
+ * @access Level 1 - get all the tasks in the project.
  *
- * @param:
- * uid - user id
- * pid - project id
+ * @param uid user id
+ * @param pid project id
  *
  * */
 module.exports.getAssociatedTasksInProject = catchAsync(
   async (req: Request, res: Response) => {
-    //TODO: Add permission control
     const uid = req.params.uid;
     const pid = req.params.pid;
 
@@ -141,12 +185,12 @@ module.exports.getAssociatedTasksInProject = catchAsync(
     const associatedTasks: any[] = [];
     // Get all task that associated to the user from the project id
     for (let i = 0; i < tasks.tasks.length; i++) {
-      let result = await Task.findById({ _id: tasks.tasks[i] });
+      let task = await Task.findById({ _id: tasks.tasks[i] });
       //TODO: If Admin reconized - return array of all results.
-      if (result) {
-        //Check if res contain the uid
-        if (result.users.includes(uid)) {
-          associatedTasks.push(result);
+      if (task) {
+        //Check if task contain the uid
+        if (task.users.includes(uid)) {
+          associatedTasks.push(task);
         }
       }
     }
